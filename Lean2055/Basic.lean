ce609e64d50ccf1b27bd4ae11dcb1f5b7dacc31c
F. G. Dorais <fgdorais@gmail.com>
2025-10-14 15:57:21 -0400

    feat: add arithmetic tactic

--- Lean2055/Basic.lean
+++ Lean2055/Basic.lean
@@ -1,1 +1,19 @@
 import Batteries
+
+namespace Lean2055
+
+scoped notation "ℕ" => Nat
+scoped notation "ℤ" => Int
+scoped notation "ℚ" => Rat
+
+attribute [scoped simp ←] Nat.mul_assoc Nat.add_assoc
+attribute [scoped simp] Nat.add_mul Nat.mul_add
+attribute [scoped simp] Nat.mul_right_comm
+attribute [scoped simp] Nat.pow_zero Nat.pow_succ
+
+attribute [scoped simp ←] Int.mul_assoc Int.add_assoc
+attribute [scoped simp] Int.add_mul Int.mul_add
+attribute [scoped simp] Int.mul_right_comm
+attribute [scoped simp] Int.neg_mul Int.pow_zero Int.pow_succ
+
+scoped macro "arithmetic" : tactic => `(tactic| first | done | simp +arith)
